62bddaab9619737bf56f46d4a472f42dd2c31294
Theorem <Theoremly@outlook.com>
2018-04-12 10:01:12 +0800

    ProjectCreated

---
+++ src/main/java/com/bugless/campus_online/CampusOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.bugless.campus_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusOnlineApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bugless/campus_online/CampusOnlineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bugless.campus_online;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CampusOnlineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
