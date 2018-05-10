e7989d544160a915fcce96a4bd9d61ea4890dab2
Theorem <Theoremly@outlook.com>
2018-05-10 17:31:51 +0800

    Add transfer value from Login to Stu/Tcher

--- src/main/java/com/bugless/campus_online/ReserveRepository.java
+++ src/main/java/com/bugless/campus_online/ReserveRepository.java
@@ -5,13 +5,13 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ReserveRepository extends JpaRepository<Reservation, Integer> {
 
     /*------------------------------------张博凯------------------------------------*/
     List<Reservation> findByTcherID(String tcherID);
-    Reservation findByStuID(String stuID);
 
     @Query(value = "select r from reservation r where r.tcher_id=?1 and r.resrv_time=?2",nativeQuery = true)
     Reservation findByTcherIDAndAndResrvTime(String tcherID,String resrvTime);
@@ -29,19 +29,23 @@ public interface ReserveRepository extends JpaRepository<Reservation, Integer> {
 
     /*------------------------------------定理------------------------------------*/
     List<Reservation> findByResrvFlag(int reserv_flag);
+    List<Reservation> findByStuID(String stuID);
     //学生提交预约
     @Query(value = "update reservation r set r.stu_id = ?1 and r.resrv_flag = ?2 where r.tcher_id = ?3 and r.resrv_time = ?4", nativeQuery = true)
     @Modifying
+    @Transactional
     public void submitReserve(String stuID, int resrv_flag, String tcher_id, String resrv_time);
 
     //学生编辑预约
     @Query(value = "update reservation r set r.tcher_id = ?1 and r.resrv_time = ?2 where r.stu_id = ?3", nativeQuery = true)
     @Modifying
+    @Transactional
     public void editReserve(String tcher_id,String resrv_time,String stu_id);
 
     //学生删除预约
-    @Query(value = "delete from reservation r where stu_id = ?1", nativeQuery = true)
+    @Query(value = "delete from reservation where stu_id = ?1", nativeQuery = true)
     @Modifying
+    @Transactional
     public void deleteReserve(String stu_id);
     /*------------------------------------定理------------------------------------*/
 }
--- src/main/java/com/bugless/campus_online/StuController.java
+++ src/main/java/com/bugless/campus_online/StuController.java
@@ -3,6 +3,7 @@ package com.bugless.campus_online;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,6 +14,9 @@ import java.lang.*;
 @RequestMapping("/student")
 public class StuController {
 
+    //用于存储从LoginController传过来的用户id
+    String current_id;
+
     @Autowired
     private ReserveRepository reserveRepository;
 
@@ -22,14 +26,24 @@ public class StuController {
 
     //获取当前可预约教师以及时间
     @RequestMapping(method = RequestMethod.GET)
-    public String getReserve(Model model) {
+    public String getReserve(@ModelAttribute("id")String ID, Model model) {
+        //接收从LoginController传过来的id
+        current_id = ID;
+        System.out.println("StuController : "+current_id);
         //reserv_flag为0代表可以预约
         List<Reservation> availableReserve = reserveRepository.findByResrvFlag(0);
-        System.out.println("GET");
+        System.out.println("StuController : GET");
         model.addAttribute("stuReserves",availableReserve);
         System.out.println("StuController : Get the reservation info");
 
+        List<Reservation> currentReserve  = reserveRepository.findByStuID(ID);
+        model.addAttribute("currentReserves", currentReserve);
+
         return "student";
+    }
+
+    public String getCurrent_id() {
+        return current_id;
     }
 
     //提交预约
