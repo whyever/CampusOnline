d2f0260fbb3e2ca8255e352e144faaea6622bcec
Kyle <592692104@qq.com>
2018-05-10 15:32:04 +0800

    temple

--- src/main/java/com/bugless/campus_online/ReserveRepository.java
+++ src/main/java/com/bugless/campus_online/ReserveRepository.java
@@ -13,14 +13,14 @@ public interface ReserveRepository extends JpaRepository<Reservation, Integer> {
     List<Reservation> findByTcherID(String tcherID);
     Reservation findByStuID(String stuID);
 
-    @Query(value = "select r from reservation r where r.tcher_id=?1 and r.resrv_time=?2",nativeQuery = true)
+    @Query(value = "select * from reservation r where r.tcher_id=?1 and r.resrv_time=?2",nativeQuery = true)
     Reservation findByTcherIDAndAndResrvTime(String tcherID,String resrvTime);
 
     @Query(value = "update reservation r set r.resrv_flag=1 where r.tcher_id=?1 and r.resrv_time=?2 ", nativeQuery = true)
     @Modifying
     public void updateReservation(String tcherID,String resrvTime);
 
-    @Query(value = "delete from reservation where id=?1 ", nativeQuery = true)
+    @Query(value = "delete from reservation r where r.id=?1 ", nativeQuery = true)
     @Modifying
     public void delete(int id);
     //http://www.cnblogs.com/hawell/p/SpringDataJpa.html
--- src/main/java/com/bugless/campus_online/TcherController.java
+++ src/main/java/com/bugless/campus_online/TcherController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,7 +33,7 @@ public class TcherController {
     }
 
     @RequestMapping(value = "/add",method = RequestMethod.POST)
-    public String add(TcherFetch tcherFetch)
+    public String add(@Valid TcherFetch tcherFetch)
     {
         Reservation reservation=new Reservation();
         //获取前端数据,每个页面增加fetch
@@ -45,11 +46,23 @@ public class TcherController {
         reserveRepository.save(reservation);
         return("add");
     }
-    @RequestMapping(value = "/select" , method = RequestMethod.POST)
-    public String select(TcherFetch tcherFetch){
-        tcherID=tcherFetch.getTcherID();
+
+    //教师进入后直接显示预约情况
+    @RequestMapping(method = RequestMethod.GET)
+    public String select( Model model){
+        tcherID="t1";
         List<Reservation> reservations=reserveRepository.findByTcherID(tcherID);
-        return("select");
+            for(Reservation res:reservations)
+            {
+                model.addAttribute("reserve.stuID",res.getStuID());
+                model.addAttribute("reserve.tcherID",res.getTcherID());
+                model.addAttribute("reserve.time",res.getResrvTime());
+                model.addAttribute("MSG",res.getResrvFlag());
+                System.out.println("message");
+            }
+
+
+        return "teacher";
     }
     //修改此项为不可预约
     @RequestMapping(value="/update" , method=RequestMethod.POST)
@@ -66,7 +79,7 @@ public class TcherController {
     @RequestMapping(value="/delete" ,method = RequestMethod.POST)
     public String delete(@Valid TcherFetch tcherFetch, Model model, BindingResult result)
     {
-        System.out.println(tcherID=tcherFetch.getTcherID());
+        /*System.out.println(tcherID=tcherFetch.getTcherID());
         System.out.println(Time=tcherFetch.getTime());
         if(result.hasErrors())
         {
@@ -76,8 +89,11 @@ public class TcherController {
         {
             model.addAttribute("MSG","right");
         }
-
+        */
+        tcherID="t1";
+        Time="2017.05.08.5";
         Reservation reservation = reserveRepository.findByTcherIDAndAndResrvTime(tcherID,Time);
+        System.out.print(reservation.getId());
         reserveRepository.delete(reservation.getId());
         return "teacher";
     }
