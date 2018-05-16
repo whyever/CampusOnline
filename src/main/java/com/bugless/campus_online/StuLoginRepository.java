05d9b1fb1f08fbce82c2256d272c266f1b547430
Theorem <Theoremly@outlook.com>
2018-05-16 15:12:42 +0800

    Stu module changed

--- src/main/java/com/bugless/campus_online/StuLoginRepository.java
+++ src/main/java/com/bugless/campus_online/StuLoginRepository.java
@@ -1,9 +1,17 @@
 package com.bugless.campus_online;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface StuLoginRepository extends JpaRepository<StuLogin, String> {
     StuLogin findByStuID(String stuID);
+
+    @Query(value = "update stu_login s set s.stu_passwd = ?1 where s.stu_id = ?2", nativeQuery = true)
+    @Modifying
+    @Transactional
+    public void changStuPassword(String newPassword, String stuId);
 }
